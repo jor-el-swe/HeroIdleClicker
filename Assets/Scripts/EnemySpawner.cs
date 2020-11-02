520abd2a23821823058bd9e5c6f3be91c4d7ad4d
Joel Ring <joelwofo@users.noreply.github.com>
2020-11-02 20:25:05 +0100

    adds template for enemy re-spawn

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -22,5 +23,18 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (_enemy.enemyHealth <= 0)
+        {
+            //do some fancy dying stuff
+            
+            
+            //spawn new enemy
+            Debug.Log("new enemy spawned");
+            _enemy.enemyHealth = scriptedEnemy.characterHealth;
+        }
     }
 }
